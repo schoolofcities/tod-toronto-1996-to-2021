a2ca10e42a7f80f7ceaca33a42cf9062df59aeff
dlm589 <dlm589@yahoo.com.tw>
2023-10-09 02:19:28 -0400

    Adding some codes for liine charts

---
+++ web-transit-analysis/transit-analysis/src/components/LineChart.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let chartWidth;
+    export let chartHeight;
+
+    const paddings = {
+        top: 50,
+        left: 50, 
+        right: 50,
+        bottom: 50,
+    };
+
+</script>
+
+<svg width = {chartWidth} height = {chartHeight}>
+<g>
+    <line 
+        x1 = {paddings.left}
+        x2 = {chartWidth - paddings.right}
+        y1 = {chartHeight - paddings.bottom}
+        y2 = {chartHeight - paddings.bottom}
+        stroke = "black"
+        stroke-width = "2" />
+
+        
+
+</g>
+</svg>
---
+++ web-transit-analysis/transit-analysis/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import LineChart from "../components/LineChart.svelte";
+    import ChartContainer from "../components/ChartContainer.svelte";
+
+</script>
+<main>
+    <ChartContainer type={"lineChart"} chartProps={{ chartWidth: 640, chartHeight: 500 }} />
+
+</main>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
