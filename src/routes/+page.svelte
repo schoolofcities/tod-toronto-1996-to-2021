3c79b8e068b8d034f12dde04d5898836111823fa
Jeff Allen <jeffallen89@gmail.com>
2024-03-01 08:40:17 -0500

    little graphic to top of page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import TopSofC from "../lib/TopSofC.svelte";
     import LineChart from "../lib/line-chart.svelte";
     import "../assets/global-styles.css";
+    import sofcLongCity from "../assets/sofc-long-city.svg";
 
     let transitName = "Line 2: Bloor-Danforth Subway";
     var variableName = "Population";
@@ -16,7 +17,7 @@
         "Row house",
         "Apartment or flat in a -plex*",
         "Apartment fewer than five storeys",
-        "Apartment five or more storeys",
+        "Apartment five or more storeys"
         // "Weighted Average number of rooms per dwelling",
         // "Owner",
         // "Renter",
@@ -49,15 +50,60 @@
         variableName = selectedValue;
         //selectedVariable.set(selectedValue);
     }
+    const lineColour = {
+        "Line 1: Yonge-University Subway": "#F1C500",
+        "Yonge North Subway Extension": "#F1C500",
+        "Line 2: Bloor-Danforth Subway": "#16A753",
+        "Scarborough Subway Extension": "#16A753",
+        "Line 3: Scarborough RT": "#1F99D5",
+        "Line 4: Sheppard Subway": "#B32078",
+        "Eglinton Crosstown LRT": "#F87005",
+        "Eglinton Crosstown West Extension": "#F87005",
+        "Eglinton East LRT": "#F87005",
+        "Eglinton Crosstown West Extension - Airport Segment": "#F87005",
+        "Finch West LRT": "#888888",
+        "Ontario Line": "#1F99D5",
+    };
+
+    let topWidth = 200;
+
+
 </script>
+
+
 
 <TopSofC />
 
 <main>
-    <div class="title">
-        <h1>
-            25 Years of Transit-Oriented Development:<br />What Has Changed?
-        </h1>
+    <div class="title" bind:clientWidth={topWidth}>
+        <img width=100% height=71 src={sofcLongCity} alt="sofcLongCity" >
+
+            <svg width="100%" height="20">
+                <line 
+                x1="2" 
+                y1="10" 
+                x2="{topWidth - 30 - 5}" 
+                y2="10" 
+                stroke={lineColour[transitName]} 
+                stroke-width="5" 
+                stroke-linecap="round"
+            />
+            {#each [0, 1, 2, 3] as c}
+                <circle
+                    r={6}
+                    cx={8 + c * ((topWidth - 30 - 16) / 3)}
+                    cy={10}
+                    stroke-width="4px"
+                    stroke={lineColour[transitName]}
+                    fill="#fff"
+                />
+            {/each}
+        </svg>
+
+        
+          
+
+        <h1>25 Years of Transit-Oriented Development:<br />What Has Changed?</h1>
 
         <p>
             <a href="">Michael Liu</a> and <a href="">Jeff Allen</a>
@@ -65,54 +111,36 @@
     </div>
 
     <div class="text">
-        <p>
-            Toronto has been encouraging building new housing units along places
-            where there is easy access to transit services, especially along
-            subway and light-rail services, usually in areas within 500 to 800
-            metres. This urban development approach is also known as
-            Transit-Oriented Developments (TOD).
-        </p>
-        <p>
-            This widget is designed to show changes along Toronto Transit
-            Commission (TTC)'s Light Rail and Subway lines, for areas within 800
-            metres (15-minute walk). You can view the changes for existing and
-            planned transit services. In addition, we have also picked a few
-            interesting aspects of TOD that we would like to explore, these
-            aspects include population, income and dwelling types.
-        </p>
-        <p>
-            The chart is currently showing the <b>{variableName}</b> that is
-            within an <b>800m</b> distance from each point on the
-            <b>{transitName}</b>
-        </p>
 
         <p>
-            <select
-                id="transit"
-                value={transitName}
-                on:change={handleTransitChange}
-            >
+            Okay! I think we need some very high level text explaining what this page is / what the widget does
+        </p>
+        <p>
+            The chart is currently showing the <b>{variableName}</b> that is within an <b>800m</b> distance from each point on the <b>{transitName}</b>
+        </p>
+
+
+        
+        <p>
+            <select id="transit" value={transitName} on:change={handleTransitChange}>
                 {#each transit_lines as value}
                     <option {value}>{value}</option>
                 {/each}
             </select>
         </p>
         <p>
-            <select
-                id="census"
-                value={variableName}
-                on:change={handleCensusChange}
-            >
+            <select id="census" value={variableName} on:change={handleCensusChange}>
                 {#each censusItems as value}
                     <option {value}>{value}</option>
                 {/each}
             </select>
         </p>
-    </div>
+
+</div>
 
     <!-- key is a function that would destroy the element and rebuilt it upon variable change-->
     {#key [transitName, variableName]}
-        <div class="chart">
+        <div class = "chart">
             <LineChart
                 variable96={variableName + " 96"}
                 variable21={variableName + " 21"}
@@ -126,67 +154,51 @@
     <div class="text">
         <h2>What Is Transit Oriented Development?</h2>
         <p>
-            Transit-oriented development (TOD) is a way of building the cities
-            around transit-infrastructures. A TOD project is usually higher
-            density, meaning that it is a more efficient use of land to house
-            people, jobs and other activities. When TOD is applied to a
-            community level, the area would be built to be pedestrian friendly
-            so its residents can have easy access to public transit. The area
-            would also be amenity rich, providing a range of services and job
-            opportunities. Through TOD, a community will become a place where
-            people can live, work, and play.
-            <br />
+            Transit-oriented development (TOD) is a way of building the cities around
+            transit-infrastructures. A TOD project is usually higher density, meaning
+            that it is a more efficient use of land to house people, jobs and other
+            activities. When TOD is applied to a community level, the area would be
+            built to be pedestrian friendly so its residents can have easy access to
+            public transit. The area would also be amenity rich, providing a range of
+            services and job opportunities. Through TOD, a community will become a place
+            where people can live, work, and play.
+            <br>
             <br /><b>What is the benefit of TOD? </b><br />
-            TOD allows people to reduce their reliance on cars for many errands.
-            For instance, a resident can simply walk to a nearby grocery store to
-            get groceries, bike a few blocks to get to the place where they are employed,
-            and access the park nearby, all without having the need to rely on cars.
-            Through reducing reliance on cars, people will not have to allocate a
-            portion of their income to buy and operate a car. TOD encourages people
-            to walk and bike more, which can in turn help people exercise, reducing
-            the level of obesity and the diseases caused by it. It is also beneficial
-            to our environment as we reduce the amount of carbon emissions and harmful
-            air pollution particles that would cause respiratory issues.
-            <br /> <br />
-            TOD is also beneficial to city governments, as TOD would locate a large
-            number of residents and jobs, which would encourage people to use public
-            transit and justify the cost of operating these systems. By building
-            more densely, cities can also cut operation and maintenance
-            costs of their infrastructures, such as roads, sewage, water, and waste
-            collection.
+            TOD allows people to reduce their reliance on cars for many errands. For instance,
+            a resident can simply walk to a nearby grocery store to get groceries, bike a
+            few blocks to get to the place where they are employed, and access the park nearby,
+            all without having the need to rely on cars. Through reducing reliance on cars,
+            people will not have to allocate a portion of their income to buy and operate
+            a car. TOD encourages people to walk and bike more, which can in turn help people
+            exercise, reducing the level of obesity and the diseases caused by it. It is
+            also beneficial to our environment as we reduce the amount of carbon emissions
+            and harmful air pollution particles that would cause respiratory issues.
+            <br /> <br>
+            TOD is also beneficial to city governments, as TOD would locate a large number
+            of residents and jobs, which would encourage people to use public transit and
+            justify the cost of operating these systems. By building more densely, cities
+            will also reduce their operation and maintenance costs of their infrastructures,
+            such as roads, sewage, water, and waste collection. For instance, 1 kilometre
+            of road can service 50 houses (or 200 people, assuming 4 people per household)
+            in a single-family community, where as 1 kilometre of road can service 2000 or
+            more people in a transit-oriented community.
         </p>
 
         <h2>Methodology</h2>
         <p>
-            Transit-Oriented Developments are defined as areas within 5-10
-            minutes walk to a transit service. In Ontario, this is equivalent to
-            500 to 800 metres. This widget compares data from <a
+            Transit-Oriented Developments are defined as areas within 5-10 minutes walk to a transit service. In Ontario, this is equivalent to 500 to 800 metres. This widget compares data from <a
                 href="https://www12.statcan.gc.ca/census-recensement/index-eng.cfm"
-                >Statistics Canada’s
-            </a>
-            Census for the year of 1996 and 2021. Census data agglomerates data to
-            multiple geographical areas, from city level data to the smallest and
-            publicly available level, Dissemination Area level data, which is consisted
-            of a few city blocks. This widget uses Dissemination Area to get the
-            most accurate estimate of the census variables. Due to the boundary of
-            800-metre buffers do not align with Dissemination Area boundaries, we
-            used areal interpolation to estimate the density of each variable within
-            respective Dissemination Areas, then interpolate the data by the area
-            that is within the 800 metre buffer. <br />
+            >Statistics Canada’s </a> Census for the year of 1996 and 2021. 
+
+            Census data agglomerates data to multiple geographical areas, from city level data to the smallest and publicly available level, Dissemination Area level data, which is consisted of a few city blocks. This widget uses Dissemination Area to get the most accurate estimate of the census variables. Due to the boundary of 800-metre buffers do not align with Dissemination Area boundaries, we used areal interpolation to estimate the density of each variable within respective Dissemination Areas, then interpolate the data by the area that is within the 800 metre buffer. <br/>
         </p>
         <p>
-            We generated points every 400 meters along transit-services in
-            Toronto, then created 800-meter buffers around the generated points.
-            Since transit stations are not 400m apart, We also used Thiessen
-            Polygon to determine the closest station for each of the point we
-            generated.
+            We generated points every 400 meters along transit-services in Toronto, then created 800-meter buffers around the generated points. Since transit stations are not 400m apart, We also used Thiessen Polygon to determine the closest station for each of the point we generated. 
             <br />
         </p>
         <p>
-            All transit layers, including the transit line and transit stations,
-            were downloaded from <a
-                href="https://www.metrolinx.com/en/about-us/open-data"
-            >
+            All transit layers, including the transit line and transit stations, were
+            downloaded from <a href="https://www.metrolinx.com/en/about-us/open-data">
                 Metrolinx's Open Data Catalogue</a
             >
         </p>
@@ -194,6 +206,7 @@
 </main>
 
 <style>
+
     select {
         padding: 3px;
         padding-left: 5px;
@@ -222,4 +235,5 @@
         margin: 0 auto;
         position: relative;
     }
+
 </style>
